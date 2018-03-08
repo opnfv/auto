8a4a37faa382003d25a71ea5b6d6dc7f1c63a434
Harry Huang <huangxiangyu5@huawei.com>
2018-03-08 14:27:48 +0800

    Test case for resiliency improvements [WIP]
    JIRA: AUTO-13
    
    On behalf of Gerard Damm to commit his codes.
    Temporarily keep resiliency test case under
    auto/testcase/resiliency.
    
    Change-Id: Idb9217e5d18113f9da69df8cce5096567655dd66
    Signed-off-by: Harry Huang <huangxiangyu5@huawei.com>

---
+++ auto/testcase/resiliency/AutoResilItfVNFMNFVO.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# OPNFV Auto project
+# https://wiki.opnfv.org/pages/viewpage.action?pageId=12389095
+
+# (c) by Gerard Damm (Wipro)
+# Use case group: Resilience Improvements
+# this module: interfaces with VNF/NVF managers (focus on ONAP)
+#     entities that manage VNFs and orchestrates services (VNF-M and NFV-O)
+
+def f1():
+    return 0
+
+
+
+
---
+++ auto/testcase/resiliency/AutoResilMain.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# OPNFV Auto project
+# https://wiki.opnfv.org/pages/viewpage.action?pageId=12389095
+
+# (c) by Gerard Damm (Wipro)
+# Use case group: Resilience Improvements
+# this module: main program
+
+#docstring
+"""
+This is the main module for OPNFV Auto Test Data for Use Caes 2: Resilience Improvements Through ONAP.
+"""
+
+######################################################################
+# import statements
+from AutoResilMgTestDef import *
+
+# Constants
+PROJECT = "Auto"
+USE_CASE_GROUP = "Resilience Improvements Through ONAP"
+
+
+######################################################################
+def main():
+
+    print(get_test_case(5))
+
+    print("End of Main\n  Project: \t\t", PROJECT, "\n  Use Case Group:\t",USE_CASE_GROUP)
+
+if __name__ == "__main__":
+    main()
+
